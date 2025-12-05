4e6b15b7d4d8d5c908daf3e9b46851f29a9dbf69
xiaopao1145 <xiaopao1145@gmail.com>
2025-12-05 22:28:44 +0800

    Initial commit

---
+++ src/main/java/io/VanillaX/vanillaXC3/VanillaXC3.java
@@ -0,0 +1,17 @@
+package io.VanillaX.vanillaXC3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanillaXC3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
